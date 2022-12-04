92c5000111d362d6c673d6fd605d7d79bfbf7e09
Zoltan Koren <korenzol.007@gmail.com>
2022-12-04 11:13:49 +0100

    Save sensordata to file

--- scripts/listenUDP.py
+++ scripts/listenUDP.py
@@ -1,4 +1,7 @@
 import socket
+from datetime import datetime
+import os
+
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 12345
@@ -12,5 +15,9 @@ while True:
     strList = data.decode('utf8', 'strict').split();
     humidity = int(strList[2]) / 1000
     temperature = int(strList[6]) / 1000
-    print("Humidity: " + str(humidity) + "%%RH")
-    print("Temperature: " + str(temperature) + "°C")
+    now = datetime.now()
+    current_time = now.strftime("%m/%d %H:%M:%S")
+    file = open('data.txt', 'a')
+    file.write(str(current_time) + "," + str(temperature) + "," + str(humidity) + "\n")
+    file.close()
+    print(str(current_time) + "\t" + str(temperature) + "\t" + str(humidity) + "\n")
